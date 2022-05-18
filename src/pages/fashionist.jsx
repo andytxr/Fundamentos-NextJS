4f75a7fcb73afbf3c0bdee40a55b8d0cbfa00f13
andytxr <andreluiiz.gst@gmail.com>
2022-05-18 18:53:33 -0300

    Modularizing the CSS

---
+++ src/pages/fashionist.jsx
@@ -0,0 +1,13 @@
+import styles from './Fashionist.module.css'
+
+export default function Fashionist(){
+
+    return (
+
+        <div className={styles.purple}>
+            <h1>Estilo usando CSS Modules</h1>
+        </div>
+
+    )
+
+}
---
+++ src/pages/jsx.jsx
@@ -0,0 +1,24 @@
+export default function JSX(){
+
+    let a = 5;
+    let b = 3;
+
+    let title = <h1>JSX é um conceito central</h1>
+
+    function subtitle(){
+
+        return <h2>{"mt legal".toUpperCase()}</h2>
+
+    }
+
+    return (
+
+        <div>
+            {title}
+            {subtitle()}
+            <h3>{a * b}</h3>
+        </div>
+
+    )
+
+}
